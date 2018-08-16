a3deb92bd2f77db4f82a342745ffa206a851db34
gabrieledarrigo <darrigo.g@gmail.com>
2018-08-16 18:43:05 +0200

    mixed config

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const config = require('dotenv').config();
+const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
 const compression = require('compression');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
-const { shorten } = require('./controllers/shorten-controller');
-
+const { shorten } = require('./api/controllers/shorten-controller');
 const {
     APPLICATION_HTTP_PORT,
     MONGO_APPLICATION_URI,
     MONGO_APPLICATION_USERNAME,
     MONGO_APPLICATION_PASSWORD
-} = config.parsed;
+} = config;
 
 const connection = mongoose.connect(MONGO_APPLICATION_URI, {
     keepAlive: true,
@@ -29,7 +28,7 @@ app.use(compression());
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 
-app.post(shorten(req, res, next));
+app.post(shorten());
 
 app.listen(APPLICATION_HTTP_PORT, () => {
     console.log(`Gab.ly application up and running on port ${APPLICATION_HTTP_PORT}`)
