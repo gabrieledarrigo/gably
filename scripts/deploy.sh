bc82e44f7e7da18b9a062e83a37d5deff5cb0e83
Gabriele D'Arrigo <darrigo.g@gmail.com>
2018-08-26 13:47:28 +0200

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo $HEROKU_API_KEY
 
-echo 'Releasing gably application'
+echo 'Pushing gably application container'
+HEROKU_API_KEY=$HEROKU_API_KEY heroku container:push web --app gably
 
-HEROKU_API_KEY=${HEROKU_API_KEY} heroku container:push web --app gably
-HEROKU_API_KEY=${HEROKU_API_KEY} heroku container:release web --app gably
+echo 'Releasing gably application container'
+HEROKU_API_KEY=$HEROKU_API_KEY heroku container:release web --app gably
