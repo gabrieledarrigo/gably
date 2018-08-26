d82f0aa21c8deccbe2de86e9846a4eeefdacd47f
Gabriele D'Arrigo <darrigo.g@gmail.com>
2018-08-26 13:52:15 +0200

    Use env variable for heroku api key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo $HEROKU_API_KEY
+echo 'Releasing gably application'
 
-echo 'Pushing gably application container'
-HEROKU_API_KEY=$HEROKU_API_KEY heroku container:push web --app gably
-
-echo 'Releasing gably application container'
-HEROKU_API_KEY=$HEROKU_API_KEY heroku container:release web --app gably
+heroku container:push web --app gably
+heroku container:release web --app gably
